89881fba77a74e2c95e7267c6fb25555651d35c6
natcha phonkamhaeng <natcha.phonkamhaeng@gmail.com>
2023-07-02 22:25:16 +0700

    update pages 2

--- src/pages/pages1.py
+++ src/pages/pages1.py
@@ -1,13 +1,24 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import Dash, html, Output, Input, dcc, callback
-import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
-import dash_ag_grid as dag
+
 
 dash.register_page(__name__, path='/', icon="bi bi-person-circle", order=1)
 
+def skill():
+	fig = go.Figure(data=go.Scatterpolar(
+		r=[4, 4, 5, 3, 3, 5],
+		theta=['Finance and Account','Coding Language','Web App Dashboard', 'English Language', 'Data Science Model', 'MS Excel'],
+		fill='toself'
+	))
+
+	fig.update_layout(polar=dict(bgcolor='rgb(30,33,48)'),showlegend=False, 
+		paper_bgcolor='rgba(0,0,0,0)',
+		margin=dict(l=45, r=45, t=45, b=45),
+		font={'color':'dodgerblue'})
+	return fig
 
 layout = dbc.Container([
 	dbc.Row([
@@ -20,28 +31,12 @@ layout = dbc.Container([
 			html.H2(['Resume']),
 			html.P(['15 years in finance / 3 years in coding / masters degree in Data Science (studying)'], style={'color': 'dodgerblue'}),
 			html.Br(),
-			html.P(['15 years experience in financing feild of Ocean Freight and Logistics Business. 3 years of coding specialized in'\
+			html.P(['15 years experience in financing field of Ocean Freight and Logistics Business. 3 years of coding specialized in'\
 				' dashboard, data cleaning and data insight with the language of Python.'], 
 				style={'justify':'center', 'color': 'grey'}),
-			html.Br(),
 			html.Hr(),
+			html.H4(children=[html.I(className='bi bi-speedometer2 me-2 icon-color'), 'Skills']),
+			dcc.Graph(figure=skill()),
 			], width=7)
 		], justify='center')
 	])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/pages/pages2.py
@@ -0,0 +1,24 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash, html, Output, Input, dcc, callback
+import plotly.graph_objects as go
+
+dash.register_page(__name__, path='/project', icon="bi bi-briefcase-fill", order=2)
+
+layout = dbc.Container([
+	dbc.Row([
+		dbc.Col([
+			html.H2(['Project']),
+			html.Br(),
+			html.H4(children=[html.I(className='bi bi-github me-2 icon-color'), 'Github Repo']),
+			html.A(children=[html.P(['https://github.com/Natcha-Phonkamhaeng'])], href='https://github.com/Natcha-Phonkamhaeng')
+			], width=12, xs=10, sm=5, md=5, lg=6, xl=5)
+		], style={'margin-left': '2rem'})
+	])
+
+
+
+
+
+
+
